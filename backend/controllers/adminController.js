86e3746bb28bf8653f5123c332b20aecd193ff2b
Krishna <srikrishna11420@gmail.com>
2026-04-24 17:33:19 +0530

    Controller Updation

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -40,3 +40,32 @@ exports.approveRequest = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+// reject request
+exports.rejectRequest = async (req, res) => {
+    try {
+        const mentorId = req.user.userId;
+        const {id} = req.params;
+         
+        const request = await OutpassRequest.findById(id);
+        if (!request) {
+            return res.status(404).json({ message: 'Request not found' });
+        }
+        if (request.mentorId.toString() !== mentorId) {
+            return res.status(403).json({ message: 'Access denied: Not your assigned request' });
+        }
+        if (request.status !== 'pending') {
+            return res.status(400).json({ message: 'Request already processed' });
+        }
+
+        request.status = 'rejected';
+        request.approvedBy = mentorId;
+        request.approvedAt = new Date();
+        await request.save();
+
+        res.json({ message: 'Request rejected successfully' }); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+};  

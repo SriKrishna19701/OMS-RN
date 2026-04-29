23db96f8a76f2613c310167c2267565163fed3eb
Krishna <srikrishna11420@gmail.com>
2026-04-29 12:58:43 +0530

    Frontend Updattion

--- OMS/src/services/api.js
+++ OMS/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Adjust the base URL as needed
+  baseURL: '192.168.240.112:5555', // Adjust the base URL as needed
 });
 
 // Request interceptor to add the auth token to each request

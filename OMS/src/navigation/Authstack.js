b1c0ee1cbc0c762fec818a4a6aeb82e08fed68ae
Krishna <srikrishna11420@gmail.com>
2026-05-02 11:22:19 +0530

    Front End Updation

--- OMS/src/navigation/Authstack.js
+++ OMS/src/navigation/Authstack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
+import RegisterScreen from '../screens/auth/RegisterScreen';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ const AuthStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
